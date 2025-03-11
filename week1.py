55617358d6d92755e1d5598b16cca10082bb73c7
Hardennes Gitonga Hargreaves B141/28494/2024 <28494@uoemdomain.embuni.ac.ke>
2025-03-11 17:08:51 +0300

    weekone

---
+++ week1.py
@@ -0,0 +1,14 @@
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+
+
+print(f"The result of {num1} + {num2} is: {addition}")
+print(f"The result of {num1} - {num2} is: {subtraction}")
+print(f"The result of {num1} * {num2} is: {multiplication}")
+
